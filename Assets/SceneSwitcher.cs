70ce5bb651de01643348cbf17029a1e51a9f9fca
Molly <mollymcconaghy@gmail.com>
2023-11-17 19:22:01 +0000

    Add a second scene

---
+++ Assets/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    //Load the next scene in the build index
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1); //or do SceneManager.LoadScene("SceneName");
+    }
+}
