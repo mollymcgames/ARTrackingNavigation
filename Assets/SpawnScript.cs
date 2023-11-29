d5f893a8223cd0a28a8004c72f38c2a9606def0e
Molly <mollymcconaghy@gmail.com>
2023-11-29 14:53:14 +0000

    spawn gems every 4 seconds

---
+++ Assets/SpawnScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject[] gems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawning()); 
+    }
+
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(4);
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(gems[i], spawnPoints[i].position, Quaternion.identity);
+        }
+
+        StartCoroutine(StartSpawning());
+    }
+}
